537344f0f4e28efeda9539788a30a9a937ece859
Martin Steen Andersen Ehlers <abo-ms@sampension.dk>
2023-12-06 11:51:12 +0100

    2023 day 6

--- Year2023/Day6.py
+++ Year2023/Day6.py
@@ -11,7 +11,11 @@ def calc_distance(T, x):
 def dist_solution(T, d):
     return ((T - math.sqrt(T ** 2 - 4 * d)) * 0.5, (T + math.sqrt(T ** 2 - 4 * d)) * 0.5)
 
-def part1():    
+def calculate_solution_range_count(T, d):
+    sols = dist_solution(T, d)
+    return math.floor(sols[1]) - math.ceil(sols[0]) + (1 if calc_distance(T, math.ceil(sols[0])) > d else -1)
+
+def part1():
     [times, distances] = [list(map(int, line.split()[1:])) for line in lines.split("\n")]
     
     product = 1
@@ -19,22 +23,15 @@ def part1():
         time = times[i]
         distance = distances[i]
         
-        sols = dist_solution(time, distance)        
-        wins = math.floor(sols[1]) - math.ceil(sols[0]) + (1 if calc_distance(time, math.ceil(sols[0])) > distance else -1)
-        
-        product *= wins
+        product *= calculate_solution_range_count(time, distance)
      
     print(product)
         
 
 def part2():    
     time = int(lines.split("\n")[0].split(":")[1].replace(" ", ""))
-    distance = int(lines.split("\n")[1].split(":")[1].replace(" ", ""))
-    
-    sols = dist_solution(time, distance)        
-    wins = math.floor(sols[1]) - math.ceil(sols[0]) + (1 if calc_distance(time, math.ceil(sols[0])) > distance else -1)
-       
-    print(wins)
+    distance = int(lines.split("\n")[1].split(":")[1].replace(" ", ""))       
+    print(calculate_solution_range_count(time, distance))
   
     
 if __name__ == "__main__":
