5266350653874ce167274800b5593300ee14f27b
steenenmartin <martinsteenandersen@gmail.com>
2024-12-10 09:18:03 +0100

    Update Day10.py

--- Year2024/Day10.py
+++ Year2024/Day10.py
@@ -10,7 +10,6 @@ inpt = """89010123
 10456732"""
 
 G = [[int(c) for c in l] for l in inpt.split()]
-
 R = len(G)
 C = len(G[0])
 
@@ -26,24 +25,22 @@ def dfs(G, v, exclude_discovered):
     discovered.append(v)
 
     if h == 9:
-        reachable_tops[z].append(v)
-        return
+        return 1
 
-    ws = []
+    res = 0
     for dr, dc in [(-1, 0), (1,0), (0, 1), (0, -1)]:
         rr, cc = r + dr, c + dc
         w = (rr, cc, h+1)
         if 0 <= rr < R and 0 <= cc < C and G[rr][cc] == h + 1 and (exclude_discovered or w not in discovered):
-            ws.append(w)
+            res += dfs(G, w, exclude_discovered)
 
-    for w in ws:
-        dfs(G, w, exclude_discovered)
+    return res
 
 
 for xclude_discovered in [False, True]:
-    reachable_tops = defaultdict(list)
+    ans = 0
     for z in zeros:
         discovered = []
-        dfs(G, z, exclude_discovered=xclude_discovered)
+        ans += dfs(G, z, exclude_discovered=xclude_discovered)
 
-    print(sum(len(reachable_tops[z]) for z in reachable_tops))
+    print(ans)
