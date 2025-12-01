53757fda1f1f9b8d26fb33ca3576390f3a0e2593
Martin Steen Andersen Ehlers <martinsteenandersen@gmail.com>
2025-12-01 09:28:21 +0100

    Create 01.py

--- 2025/01.py
+++ 2025/01.py
@@ -11,25 +11,18 @@ L82"""
 
 dial = 50
 password = 0
-count_during_rotations = True
+count_during_rotations = False
 
 rotations = [(1 if line[0] == "R" else -1, int(line[1:])) for line in inpt.split()]
 
 for direction, rotation in rotations:
-    rotation_mod_100 = rotation % 100
-
-    if direction == -1 and dial - rotation < 0:
-        password += 1
-        dial = 100 + (dial - rotation)
-    else:
-        dial += direction * rotation
+    new_dial = dial + direction * rotation
 
     if count_during_rotations:
-        password += (rotation - rotation_mod_100) // 100
-        if dial >= 100:
-            dial %= 100
-            password += 1
+        password += abs(new_dial // 100 - dial // 100)
     else:
-        password += (dial == 0)
+        password += (new_dial % 100 == 0)
+
+    dial = new_dial
 
 print(password)
